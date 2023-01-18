b70994e13fe7ab4c9f07f11a13831e9b21e7a4e4
Udaya <122195409+udayavanan9@users.noreply.github.com>
2023-01-18 16:36:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function Toggle(){
+    let x = document.querySelector("#demo");
+    x.style.color="red";
+}
+const btn=document.getElementById("btn");
+btn.addEventListener('click',function(){
+    alert("Register again");
+});
